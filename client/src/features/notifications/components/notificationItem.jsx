1609c73d21095b2bad57adde3092e7cffe2891af
Arpan <arpanthe123@gmail.com>
2026-06-26 17:21:50 +0530

    fix no 2

--- client/src/features/notifications/components/notificationItem.jsx
+++ client/src/features/notifications/components/notificationItem.jsx
@@ -1,7 +1,6 @@
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-
-import NotificationIcon from "./NotificaionIcon";
+import NotificationIcon from "./NotificationIcon";
 
 dayjs.extend(relativeTime);
 
